2325df7dc1c24b1885221d578fae13195afd9ac2
Aaron Schcolnik <aaron.schcolnik@uabc.edu.mx>
2023-04-18 12:03:24 -0600

    18 de abril

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,23 +1,44 @@
 import { useState, useEffect } from 'react';
-import { getGifs } from '../helpers/getGifs';
+import { useFetchGifs } from './hooks/useFetchGifs';
+import { GifItem } from './GifItem';
 
 export const GifGrid = ({ category }) => {
-    
-    const [counter, setCounter] = useState(10);
 
-    useEffect( () => {
-        
-        getGifs(category); // esto dice "corre getGifs unicamente cuando cambio la categoria"
+    const {images, isLoading} = useFetchGifs( category );
 
-    }, [ ])
+    // console.log({isLoading});
+
+    // Todo este codigo sera resumido en un custom hook llamado useFetchGifs
+    // const[images, setImages] = useState([]);
+
+    // const getImages = async() => {
+    //     const newImages = await getGifs(category);
+    //     setImages(newImages);
+    // }
+
+    // useEffect( () => {
+    //     getImages();
+    // }, [ ])
 
 
     return (
         <>
-            <h3>{category}</h3>   
+            <h3>{category}</h3>
+            {
+                isLoading && ( <h2>Cargando...</h2> ) //if isLoading es true, imprime el h2
+            }
 
-            <h5>{ counter }</h5>
-            <button onClick={ () => setCounter( counter + 1 ) }>+1</button>
+            <div className="card-grid">
+                {
+                images.map( (image) => (
+                        <GifItem 
+                            key= { image.id }
+                            {...image} //esto esparce todas las properties de image
+                        />
+                    ))
+                }
+            </div> 
+
         </>
     )
 }
